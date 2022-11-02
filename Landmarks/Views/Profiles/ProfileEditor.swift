d0cd8ea363dbfcb56c295f647f3df378a51675e5
hertz315 <hertz315@gmail.com>
2022-11-02 15:53:46 +0900

    app design and layout update

---
+++ Landmarks/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,56 @@
+//
+//  ProfileEditor.swift
+//  Landmarks
+//
+//  Created by Hertz on 11/2/22.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    @Binding var profile: Profile
+    
+    var dateRange: ClosedRange<Date> {
+        get{
+            let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalData)!
+            let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalData)!
+            return min...max
+        }
+    }
+    
+    var body: some View {
+        List {
+            HStack {
+                Text("Username").bold()
+                Divider()
+                TextField("Username", text: $profile.username)
+            }
+            
+            Toggle(isOn: $profile.prefersNotifications) {
+                Text("Enable Notifications").bold()
+            }
+            
+            VStack(alignment: .leading) {
+                Text("Seasonal Photo").bold()
+                
+                Picker("Seasonal Photo", selection: $profile.seasonalPhoto) {
+                    ForEach(Profile.Season.allCases) { season in
+                        Text(season.rawValue).tag(season)
+                        
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            DatePicker(selection: $profile.goalData, in: dateRange, displayedComponents: .date) {
+                Text("Goal Date").bold()
+            }
+        }
+    }
+}
+
+struct ProfileEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditor(profile: .constant(.default))
+    }
+}
